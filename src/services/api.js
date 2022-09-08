6a23d914b492c2b842c8a224113bc286eccd8d37
caiobacode <caioodsf@gmail.com>
2022-09-08 15:37:27 -0300

    Completo

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,20 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const data = await fetch(url);
+  const response = data.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/${categoryId}/${query}`;
+  const data = await fetch(url);
+  const response = data.json();
+  return response;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(query) {
+  const url = `https://api.mercadolibre.com/${query}`;
+  const data = await fetch(url);
+  const response = data.json();
+  return response;
 }
